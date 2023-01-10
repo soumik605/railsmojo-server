576bd134897f5aee6f340e70e3048d84cad85aa6
Avishek Jana <avishek@geogo.in>
2023-01-10 16:59:48 +0530

    Flowbite configured

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   resources :categories
   resources :rails_versions
   devise_for :users
-  get 'home/index'
+  get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
+  root "pages#home"
 end
