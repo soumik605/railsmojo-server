d768d1e955e2a0b85ca9583aef356e25206f21ff
Soumik Maity <soumik.maity@geogo.in>
2023-02-06 13:54:52 +0530

    added CRUD in articles, categories and rails versions

---
+++ app/controllers/admin/articles_controller.rb
@@ -0,0 +1,71 @@
+class Admin::ArticlesController < ApplicationController
+  require 'octokit'
+  layout 'admin'
+
+  before_action :set_article, only: %i[ show edit update destroy ]
+
+  def index
+    @articles = Article.all
+    @articles = @articles.paginate(:page => params[:page], :per_page => 10)
+
+    @article = Article.new
+    @rails_versions = RailsVersion.all.active
+    @categories = Category.all.active
+  end
+
+  def show
+    client = Octokit::Client.new(:access_token => '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')
+    @response = client.contents('soumik605/soumik605', path: @article.github_link, query: {},  :accept => 'application/vnd.github.html')
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to admin_articles_url, notice: "Article was successfully created." }
+        format.json { render :show, status: :created, location: @article }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @article.update(article_params)
+        format.html { redirect_to admin_articles_url, notice: "Article was successfully updated." }
+        format.json { render :show, status: :ok, location: @article }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @article.destroy
+
+    respond_to do |format|
+      format.html { redirect_to admin_articles_url, notice: "Article was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:category_id, :rails_version_id, :title, :key, :github_link, :author_name, :piblished_at)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  resources :articles
-  resources :categories
-  resources :rails_versions
+
+  namespace :admin do
+    resources :articles
+    resources :categories
+    resources :rails_versions
+  end
+
+  resources :articles, only: [:index, :show]
+  get 'admin' => 'admin/articles#index'
+  
   devise_for :users
   get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
